30a6f20ba33b5f8e2bf172626a37c5bf2d51de10
LonelyFellas <289582304@qq.com>
2023-02-25 21:41:40 +0800

    unit3_practice

---
+++ unit3_practice/target/debug/deps/unit3_practice-69443047894fb980.d
@@ -0,0 +1,5 @@
+/Users/darwish/Desktop/rust/unit3_practice/target/debug/deps/unit3_practice-69443047894fb980: src/main.rs
+
+/Users/darwish/Desktop/rust/unit3_practice/target/debug/deps/unit3_practice-69443047894fb980.d: src/main.rs
+
+src/main.rs:
---
+++ unit3_practice/target/debug/unit3_practice.d
@@ -0,0 +1,1 @@
+/Users/darwish/Desktop/rust/unit3_practice/target/debug/unit3_practice: /Users/darwish/Desktop/rust/unit3_practice/src/main.rs
