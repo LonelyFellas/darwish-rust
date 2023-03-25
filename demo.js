86db80261f39b3b6bfc7536e0cdb034c190a78d8
LonelyFellas <289582304@qq.com>
2023-03-25 22:46:23 +0800

    hua

--- demo.js
+++ demo.js
@@ -1,3 +1,21 @@
-fn main() {
-  
-}
+var majorityElement = function(nums) {
+  if (nums.length === 1) return nums[0];
+
+  let map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    let current_value = map.get(nums[i]);
+    if (current_value === undefined) {
+        map.set(nums[i], 1);
+    } else {
+        map.set(nums[i], current_value+1)
+        if (current_value + 1 > nums.length / 2) {
+            return nums[i];
+        }
+    }
+  }
+};
+
+
+const a = majorityElement([2,2,1,1,1,2,2]);
+console.log(a);
--- leetcode/src/main.rs
+++ leetcode/src/main.rs
@@ -1,53 +1,53 @@
-// fn main () {
-//     let vector: Vec<String> = vec!["++X".to_string(), "X++".to_string(), "--X".to_string(), "X++".to_string()];
+// // fn main () {
+// //     let vector: Vec<String> = vec!["++X".to_string(), "X++".to_string(), "--X".to_string(), "X++".to_string()];
 
-//     for v in vector {
-//         let operate = match v.as_str() {
-//             "++X" => 1,
-//             "X++" => 1,
-//             "--X" => -1,
-//             "X--" => -1,
-//             _ => 0,
-//         };
+// //     for v in vector {
+// //         let operate = match v.as_str() {
+// //             "++X" => 1,
+// //             "X++" => 1,
+// //             "--X" => -1,
+// //             "X--" => -1,
+// //             _ => 0,
+// //         };
 
-//         initValue += operate;
-//     }
+// //         initValue += operate;
+// //     }
 
+// // }
+// fn main() {
+//   let strs = vec![String::from("flower"),String::from("flow"),String::from("flight")];
+//   println!("{:?}", longest_common_prefix(strs))
 // }
-fn main() {
-  let strs = vec![String::from("flower"),String::from("flow"),String::from("flight")];
-  println!("{:?}", longest_common_prefix(strs))
-}
-pub fn longest_common_prefix(strs: Vec<String>) -> String {
-  // let mut min_len = strs[0].len();
-  // let mut index = 0;
-  // let mut count = 1;
+// pub fn longest_common_prefix(strs: Vec<String>) -> String {
+//   // let mut min_len = strs[0].len();
+//   // let mut index = 0;
+//   // let mut count = 1;
 
-  // loop {
-  //   if min_len > strs[count].len() {
-  //     min_len = strs[count].len();
-  //     index = count;
-  //   }
+//   // loop {
+//   //   if min_len > strs[count].len() {
+//   //     min_len = strs[count].len();
+//   //     index = count;
+//   //   }
 
-  //   if count >= strs.len() - 1 {
-  //     break;
-  //   }
-  //   count += 1;
-  // }
-  // println!("{index}");
+//   //   if count >= strs.len() - 1 {
+//   //     break;
+//   //   }
+//   //   count += 1;
+//   // }
+//   // println!("{index}");
 
-  // for c in strs[index].chars() {
+//   // for c in strs[index].chars() {
 
-  // }
-  let a: String = strs.iter()
-  .max()
-  .unwrap()
-  .chars()
-  .zip(strs.iter().min().unwrap().chars())
-  .take_while(|x| x.0 == x.1)
-  .map(|x| x.0)
-  .collect();
-  println!("{:?}", a);
+//   // }
+//   let a: String = strs.iter()
+//   .max()
+//   .unwrap()
+//   .chars()
+//   .zip(strs.iter().min().unwrap().chars())
+//   .take_while(|x| x.0 == x.1)
+//   .map(|x| x.0)
+//   .collect();
+//   println!("{:?}", a);
 
-  return "--------hello".to_string();
-}
+//   return "--------hello".to_string();
+// }
